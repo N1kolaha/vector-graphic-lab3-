d1481560338ea17159ef86dc88fbf3243167f762
N1kolaha <145463329+N1kolaha@users.noreply.github.com>
2023-11-24 14:40:08 +1000

    Add files via upload

--- OOp(vectors)/OOp(vectors).cpp
+++ OOp(vectors)/OOp(vectors).cpp
@@ -52,7 +52,7 @@ struct Circle: Figure {
         return os << "<circle cx=\"" << c.cx << "\" cy=\"" << c.cy << "\" r=\"" << c.r  << "\" fill=\"" << c.fill << "\" stroke=\"" << c.stroke << "\" stroke-width=\"" << c.stroke_width << "\" />";
     }
 
-struct Poligon : Figure {
+struct Polygon : Figure {
     double x1;
     double y1;
     double x2;
@@ -62,7 +62,7 @@ struct Poligon : Figure {
     double x4;
     double y4;
     std::string fill;
-    Poligon(double x_1, double y_1, double x_2, double y_2, double x_3, double y_3, double x_4, double y_4, std::string _fill, std::string _stroke) {
+    Polygon(double x_1, double y_1, double x_2, double y_2, double x_3, double y_3, double x_4, double y_4, std::string _fill, std::string _stroke) {
         x1 = x_1;
         y1 = y_1;
         x2 = x_2;
@@ -75,7 +75,7 @@ struct Poligon : Figure {
         stroke = _stroke;
     }
 };
-     std::ostream& operator<<(std::ostream& os, const Poligon& p) {
+     std::ostream& operator<<(std::ostream& os, const Polygon& p) {
         return os << "<polygon points=\"" << p.x1 << "," << p.y1 << " " << p.x2 << "," << p.y2 << " " << p.x3 << "," << p.y3 << " " << p.x4 << "," << p.y4 << "\" fill=\"" << p.fill << "\" stroke=\"" << p.stroke << "\" />";
     }
 
@@ -92,9 +92,9 @@ int main() {
     Circle circle(400, 400, 350,  "none","black",25);
     
     Circle circle1(565, 195, 40, "purple", "black", 1);
-    Poligon pol1(100, 0, 250, 0, 800, 450, 800, 750, "#75998D","black");
+    Polygon pol1(100, 0, 250, 0, 800, 450, 800, 750, "#75998D","black");
 
-    Poligon pol2(0, 600, 0, 800, 800, 0,750, 0, "#C5AE7C","black");
+    Polygon pol2(0, 600, 0, 800, 800, 0,750, 0, "#C5AE7C","black");
    
     svg << "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"800\" height=\"800\"  style=\"background-color: #DAD0BF;\">";
     svg << pol1  << pol2  << circle <<circle1<< line;
